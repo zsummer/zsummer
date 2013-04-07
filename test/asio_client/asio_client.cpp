2f1457675a884ee7f0bdee75efa01605bb3e14b7
zhangyawei <yawei_zhang@foxmail.com>
2013-04-07 21:28:21 +0800

    asio test client clear

--- test/asio_client/asio_client.cpp
+++ test/asio_client/asio_client.cpp
@@ -1,7 +1,12 @@
 // asio_client.cpp : 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
+#define _WIN32_WINNT 0x0501
+#define WIN32_LEAN_AND_MEAN	
+
+
+#include <stdio.h>
+#include <tchar.h>
 #include <iostream>
 #include <string>
 #include <boost/asio.hpp>
@@ -12,7 +17,7 @@ using namespace std;
 using namespace boost;
 
 #define _BUF_LEN	(5*1024)
-#define _SEND_LEN   (1024)
+#define _SEND_LEN   (200)
 
 static int g_recvmsgs = 0;
 static char g_senddata[_BUF_LEN];
@@ -21,14 +26,16 @@ static char g_senddata[_BUF_LEN];
 class CClient
 {
 public:
-	CClient(asio::io_service & ios): m_ios(ios), m_socket(m_ios), m_time(m_ios)
+	CClient(asio::io_service & ios, std::string ip, unsigned short port): 
+	  m_ios(ios),
+	  m_ip(ip),
+	  m_port(port),
+	  m_socket(m_ios),
+	  m_time(m_ios)
 	{
 		memset(m_recvbuf, 0 , sizeof(m_recvbuf));
-
 		m_type = 0;
-
-		//m_time.expires_from_now(boost::posix_time::milliseconds(1000));
-		m_time.expires_from_now(boost::posix_time::milliseconds(2000+rand()%30000));
+		m_time.expires_from_now(boost::posix_time::milliseconds(1000+rand()%30000));
 		m_time.async_wait(boost::bind(&CClient::TimeOut, this, _1));
 	}
 	bool Connect(const char * ip, unsigned short port)
@@ -36,7 +43,6 @@ public:
 		m_socket.async_connect(asio::ip::tcp::endpoint(asio::ip::address_v4::from_string(ip), port), boost::bind(&CClient::connect_handler, this, _1));
 		return true;
 	}
-
 	void connect_handler(const boost::system::error_code& error)
 	{
 		if (error)
@@ -46,7 +52,6 @@ public:
 		}
 		else
 		{
-			//cout <<" connect ok" << endl;
 			Send();
 			Recv();
 		}
@@ -56,38 +61,23 @@ public:
 		m_socket.async_receive(asio::buffer(m_recvbuf, _SEND_LEN), boost::bind(&CClient::OnRecv, this, _1));
 		return true;	
 	}
-
 	void OnRecv(const boost::system::error_code& error)
 	{
 		if (error)
 		{
 			cout <<"ERR: "  << boost::system::system_error(error).what() << endl;
 			m_socket.close();
-
 			return ;
 		}
-
-
 		if (memcmp(m_recvbuf, g_senddata, _SEND_LEN) != 0)
 		{
 			cout << "ERR: recv err:  datalen=" <<*((unsigned short*)m_recvbuf) << " data="  <<m_recvbuf+2 << endl;
 			m_socket.close();
-
 			return;
 		}
-
 		g_recvmsgs++;
-
- 		//cout << " recv data: " << m_recvbuf+2 << endl;
-
 		Recv();
 	}
-
-
-
-
-
-
 	bool Send()
 	{
 		m_socket.async_send(asio::buffer(g_senddata, _SEND_LEN), boost::bind(&CClient::OnSend, this, _1));
@@ -116,19 +106,17 @@ public:
 		if (m_type == 0)
 		{
 			m_type = 99;
-			//Connect("192.168.138.58", 81);
-			//Connect("192.168.138.146", 81);
-			//Connect("192.168.204.128", 81);
-			Connect("127.0.0.1", 81);
+			Connect(m_ip.c_str(), m_port);
 		}
 		else
 		{
 			Send();
 		}
-		
 	}
 private:
 	asio::io_service & m_ios;
+	std::string m_ip;
+	unsigned short m_port;
 	asio::ip::tcp::socket m_socket;
 	asio::deadline_timer m_time;
 	int m_type;
@@ -152,9 +140,6 @@ void Moniter(const boost::system::error_code& error )
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-
-
-	
 	{
 		memset(g_senddata, 0, sizeof(g_senddata));
 		char tmpt[] = "001234567890abcdefghijklmnopqrstuvwxyz";
@@ -171,7 +156,6 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 		unsigned short len = _SEND_LEN;
 		memcpy(g_senddata, &len, 2);
-
 	}
 
 
@@ -184,13 +168,26 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	srand(time(NULL));
 
-	cout << " enter a num for clients" << endl;
-	int maxn =0;
+	std::string ip;
+	unsigned short port = 0;
+	int maxn = 0;
+	cout << "zsummer's server ip:" << endl;
+	cin >> ip;
+	cout << "zsummer's server port:" << endl;
+	cin >> port;
+	cout << "stress client num:" << endl;
 	cin >> maxn;
-	cout <<" will create " << maxn << " clts" << endl;
+	cout <<" zsummer's server info: [" << ip << ":" << port << "]  stress clients:" << maxn << endl;
+	cout << "press any key to continue.";
+	if (!cin)
+	{
+		return -1;
+	}
+	getchar();
+	getchar();
 	for (int i=0; i<maxn; i++)
 	{
-		CClient * p = new CClient(ios);
+		CClient * p = new CClient(ios, ip, port);
 	}
 
 	
